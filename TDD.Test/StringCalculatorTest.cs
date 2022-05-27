1ab4ec41eb18b4a29284f95240642b167a43ce98
DZIANIS KAHUKIN <dzianis.kahukin@innowise-group.com>
2022-05-27 09:59:38 +0300

    Add project files.

---
+++ TDD.Test/StringCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TDD.Test
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void Add_EmptyString_returnZero()
+        {
+            var stringCalculator = new StringCalculator();
+            Assert.Equal(0, stringCalculator.Add(string.Empty));
+        }
+    }
+}
---
+++ TDD/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace TDD
+{
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            number = "3";
+            return int.TryParse(number, out int result) ? result : 0;
+        }
+    }
+}
